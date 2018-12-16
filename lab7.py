cb5414299aec36b1331d9affe43d9a2265cf48d4
mohammad20-meet <mohammad20@meet.mit.edu>
2018-12-16 12:51:01 -0500

    lab7

--- lab7.py
+++ lab7.py
@@ -2,9 +2,22 @@ from turtle import *
 import random
 import turtle
 import math
-colors = ["blue","green","red"]
+turtle.colormode(255)
+#edges
+width = 350
+height = 350
+
+
+turtle.pu()
+turtle.goto(350,0)
+turtle.pd()
+turtle.goto(350,350)
+turtle.goto(-350,350)
+turtle.goto(-350,-350)
+turtle.goto(350,-350)
+turtle.goto(350,0)
 class Ball(Turtle):
-	def __init__(self,x,y,dx,dy,radius, color):
+	def __init__(self,x,y,dx,dy,radius,color):
 		Turtle.__init__(self)
 		self.shape("circle")
 		self.shapesize(radius/10)
@@ -18,24 +31,45 @@ class Ball(Turtle):
 		oldx = self.xcor()
 		oldy = self.ycor()		
 		newx = oldx + self.dx
-		newy = newy + self.dy
+		newy = oldy + self.dy
 		self.goto(newx,newy)
+		if newx>width or newx<-width:
+			self.dx = -self.dx
+		if newy>height or newy<-height:
+			self.dy = -self.dy
+	def random_color(self):
+		r = random.randint(0,256)
+		g = random.randint(0,256)
+		b = random.randint(0,256)
+		self.color(r,g,b)
+
+
 def check_colliision(ball_1,ball_2):
-	d=math.sqrt(math.pow(x2-x1),2) + math.pow((y2-y1))
-	if d<ball_1.radius+ball_2.radius:
+	x1 = ball_1.xcor()
+	x2 = ball_2.xcor()
+	y1 = ball_1.ycor()
+	y2 = ball_2.xcor()
+	D = math.sqrt(math.pow((x2-x1),2) + math.pow((y2-y1),2))
+
+	if D<ball_1.radius+ball_2.radius:
 		return True
 	else:
 		return False
 
-ball_1 = Ball(50,30,50,30,15,"red")
-ball_2 = Ball(50,14,20,40,15,"blue")
-'''
-if(check_colliision(ball_1, ball_2) == True):
-	random_color = colors.choice()
-	ball_1.color(random_color)
-	ball_2.color(random_color)
-'''
 
+
+turtle.bgcolor("pink")
+ball_1 = Ball(30,30,50,30,30,"blue")
+ball_2 = Ball(50,14,20,40,30,"red")
+
+
+
+while True:
+	ball_1.move()
+	ball_2.move()
+	if(check_colliision(ball_1, ball_2) == True):
+		ball_1.random_color()
+		ball_2.random_color()
 turtle.mainloop()
 
 
