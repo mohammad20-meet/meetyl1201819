a4cd611cba63dc07bc58ac222fbae9493bfb0643
mohammad20-meet <mohammad20@meet.mit.edu>
2019-01-13 12:21:11 -0500

    agar.io,ball.py

--- agario.py
+++ agario.py
@@ -2,7 +2,7 @@ import turtle
 import time
 import random
 from ball import *
-turtle.tracer(0)
+turtle.tracer(0, 0)
 turtle.hideturtle()
 RUNNING = True
 SLEEP = 0.0077
@@ -12,15 +12,59 @@ SCREEN_HEIGHT = turtle.getcanvas().winfo_height()/2
 My_BALL =  Ball(10,-30,-50,30,30,"green")
 NUMBER_OF_BALLS = 5
 MINIMUM_BALL_RADIUS = 10
-MAXIMUM_BALL_RADIUS = 100
+MAXIMUM_BALL_RADIUS = 70
 MINIMUM_BALL_DX = 5
 MAXIMUM_BALL_DX = 5
-MINIMUM_BALL_DY = -5
+MINIMUM_BALL_DY = 5
 MAXIMUM_BALL_DY = 5
+
+
 BALLS = []
+death = []
+for i in range(NUMBER_OF_BALLS):
+	x = random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS,SCREEN_WIDTH - MAXIMUM_BALL_RADIUS)
+	y = random.randint(-SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS,SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)
+	dx = random.randint(MINIMUM_BALL_DX,MAXIMUM_BALL_DX)
+	dy = random.randint(MINIMUM_BALL_DY,MAXIMUM_BALL_DY)
+	radius = random.randint(MINIMUM_BALL_RADIUS,MAXIMUM_BALL_RADIUS)
+	color = (random.random(),random.random(),random.random())
+	new_ball = Ball(x,y,dx,dy,radius,color)
+	BALLS.append(new_ball)
+
+def move_all_balls():
+	for i in BALLS:
+		i.move(SCREEN_WIDTH,SCREEN_HEIGHT)
+		turtle.update()
 
 
 
 
-turtle.update()
-turtle.mainloop()
+def collide(ball_a,ball_b):
+	x1 = ball_a.xcor()
+	x2 = ball_b.xcor()
+	y1 = ball_a.ycor()
+	y2 = ball_b.xcor()
+	if ball_a == ball_b:
+		return False
+	D = math.sqrt(math.pow((x2-x1),2) + math.pow((y2-y1),2))
+	if D+10<ball_a.radius+ball_b.radius:
+		return True
+	else:
+		return False
+def check_all_balls_collision():
+	for ball_a in BALLS:
+		for ball_b in BALLS:
+			if(collide(ball_a, ball_b) == True):
+				if ball_a.radius> ball_b.radius:
+					ball_a.grow(ball_b.radius)
+					ball_b.thxforplaying(SCREEN_WIDTH,SCREEN_HEIGHT)
+				elif ball_b.radius>ball_a.radius:
+					ball_b.grow(ball_a.radius)
+					ball_a.thxforplaying(SCREEN_WIDTH,SCREEN_HEIGHT)
+
+
+while RUNNING:
+	move_all_balls()
+	time.sleep(SLEEP)
+	check_all_balls_collision()
+	turtle.update()
--- ball.py
+++ ball.py
@@ -1,10 +1,25 @@
 from turtle import *
 import random
 import math
-# turtle.pu()
+NUMBER_OF_BALLS = 5
+MINIMUM_BALL_RADIUS = 10
+MAXIMUM_BALL_RADIUS = 70
+MINIMUM_BALL_DX = 5
+MAXIMUM_BALL_DX = 5
+MINIMUM_BALL_DY = -5
+MAXIMUM_BALL_DY = 5
+
+NUMBER_OF_DEATH = 5
+MINIUM_DEATH_RADIUS = 10
+MAXIMUM_DEATH_RADIUS = 70
+MINIMUM_DEATH_DX = 5
+MAXIMUM_DEATH_DX = 5
+MINIMUM_DEATH_DY = -5
+MAXIMUM_DEATH_DY = 5
 class Ball(Turtle):
 	def __init__(self,x,y,dx,dy,radius,color):
 		Turtle.__init__(self)
+		self.pu()
 		self.shape("circle")
 		self.shapesize(radius/10)
 		self.radius = radius
@@ -33,6 +48,45 @@ class Ball(Turtle):
 
 		if bottom_side_ball<-height:
 			self.dy = -self.dy
+	def speed(self):
+		if 15>ball.radius>10:
 
+		elif 20>ball.radius>16:
+
+		elif 25>ball.radius>21:
+
+		elif 30>ball.radius>26:
+
+		elif 35>ball.radius>31:
+
+		elif 40>ball.radius>36:
+
+		elif 45>ball.radius>41:
+
+		elif 50>ball.radius>46:
+
+		elif 55>ball.radius>51:
+
+		elif 60>ball.radius>56:
+
+		elif 65>ball.radius>61:
+
+		elif 70>ball.radius>66:
+			
+	def grow(self, radius):
+		self.radius += radius
+		self.shapesize(self.radius/10)
+		if self.radius > MAXIMUM_BALL_RADIUS:
+			self.shapesize(MAXIMUM_BALL_RADIUS/10)
+
+	def thxforplaying(self, SCREEN_WIDTH, SCREEN_HEIGHT):
+		x = random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS,SCREEN_WIDTH - MAXIMUM_BALL_RADIUS)
+		y = random.randint(-SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS,SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)
+		radius = random.randint(MINIMUM_BALL_RADIUS,MAXIMUM_BALL_RADIUS)
+		color = (random.random(),random.random(),random.random())
+		self.goto(x,y)
+		self.color(color)
+		self.shapesize(radius/10)
+		self.radius = radius
 
 
